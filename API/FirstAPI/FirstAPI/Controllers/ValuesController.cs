48a9fa8f68359a42646fe194f17f1a277310fdde
Nick <nikitos.aka.zed@gmail.com>
2018-08-06 17:29:55 +0300

    API

---
+++ API/FirstAPI/FirstAPI/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var connection = new System.Data.SqlClient.SqlConnection(@"server=127.0.0.1, 3306;persistsecurityinfo=True;Database=sportsstore;user=root;password=Qwerty789#");
+            string sql = "Select * FROM products";
+
+            var productsList = connection.Query(sql).ToList();
+
+            Console.WriteLine(productsList);
+
+            return new string[] { productsList.ToString() };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

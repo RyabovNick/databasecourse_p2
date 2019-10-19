ab19d272bf983dccac0ed504c0851af3b67204f2
RyabovNick <nikitos.aka.zed@gmail.com>
2019-10-19 15:09:41 +0300

    add maxChar & reverseInt

--- NodeJS/Tasks/README.md
+++ NodeJS/Tasks/README.md
@@ -3,3 +3,7 @@
 `npm install`
 
 `npm run test` - для запуска тестов
+
+Если необходимо запускать тест для конкретного задания, то в package.json поменяйте `jest chunk/test --watch` на `jest maxchar/test --watch` (запускать тесты для задания maxchar)
+
+Можно дебажить код, нажав f5
---
+++ NodeJS/Tasks/reverseInt/index.js
@@ -0,0 +1,13 @@
+// --- Задание
+// Входной параметр функции число Int, которая
+// возвращает число с обратной стороны
+// --- Примеры
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {}
+
+module.exports = reverseInt

4f3fd6468d2a16d778f2ef571aaa6d0ec2e5ea41
RyabovNick <nikitos.aka.zed@gmail.com>
2019-10-26 12:28:59 +0300

    add steps & capitalize tasks

---
+++ NodeJS/Tasks/capitalize/index.js
@@ -0,0 +1,12 @@
+// --- Directions
+// Напишите функцию, которая принимает string.
+// Функция должна вернуть такую же string, только изменив
+// первый символ в каждом слове на большой.
+// --- Примеры
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+function capitalize(str) {}
+
+module.exports = capitalize

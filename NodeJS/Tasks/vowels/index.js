4279afc426de504bf3bbf74274a462db1a4ffdfe
RyabovNick <nikitos.aka.zed@gmail.com>
2019-10-26 11:33:49 +0300

    add vowels task

---
+++ NodeJS/Tasks/vowels/index.js
@@ -0,0 +1,11 @@
+// --- Directions
+// Напишите функцию, которая возвращает кол-во согласных: 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Пример
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+function vowels(str) {}
+
+module.exports = vowels

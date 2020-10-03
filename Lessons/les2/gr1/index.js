47e63bc90262c24ea0762796640b0b21f1ea0681
RyabovNick <nikitos.aka.zed@gmail.com>
2020-10-03 16:04:30 +0300

    upd gr1 les2

--- Lessons/les2/gr1/index.js
+++ Lessons/les2/gr1/index.js
@@ -8,20 +8,82 @@ const client = new Client({
     port: process.env.DB_PORT,
 })
 
-const id = '1'
+/**
+ * Оформление нового заказа
+ * 1. Создаем новый заказ и получем его ID
+ * 2. Подсчитываем цену заказа
+ * 3. Каждый товар из заказа добавить в таблицу order_menu
+ */
+async function createOrder() {
+    client.connect()
+    const order = {
+        clientID: 1,
+        menu: {
+            id: 1,
+            count: 2,
+        },
+        // menu: [
+        //     {
+        //         id: 1,
+        //         count: 1,
+        //     },
+        //     {
+        //         id: 3,
+        //         count: 3,
+        //     },
+        // ],
+    }
 
-client.connect()
-client
-    .query(
-        `
-    SELECT *
-    FROM test
-    WHERE id = $1`,
-        [id]
+    // Создали заказ и получили его ID
+    const resOrderID = await client.query(
+        `INSERT INTO order_ (client_id) VALUES ($1) RETURNING id`,
+        [order.clientID]
     )
-    .then((result) => console.log(result.rows))
-    .catch((e) => console.error(e.stack))
-    .then(() => client.end())
+    const orderID = resOrderID.rows[0].id
+    console.log('new order: ', orderID)
+
+    const resPrice = await client.query(
+        `
+    SELECT id, price::numeric
+    FROM menu
+    WHERE id = $1
+    `,
+        [order.menu.id]
+    )
+
+    const price = resPrice.rows[0].price * order.menu.count
+    console.log('price:', price)
+
+    await client.query(
+        'INSERT INTO order_menu (order_id, menu_id, count, price) VALUES ($1, $2, $3, $4)',
+        [orderID, order.menu.id, order.menu.count, price]
+    )
+
+    await client.end()
+}
+
+createOrder()
+    .then(() => {
+        console.log('success')
+    })
+    .catch((err) => {
+        console.log('error', err)
+    })
+
+// const id = '1'
+
+// client.connect()
+// client
+//     .query(
+//         `
+//     SELECT *
+//     FROM test
+//     WHERE id = $1`,
+//         [id]
+//     )
+//     .then((result) => console.log(result.rows))
+//     .catch((e) => console.error(e.stack))
+//     .then(() => client.end())
 // ctrl+k+c
 // ctrl+k+u
 // client.query(

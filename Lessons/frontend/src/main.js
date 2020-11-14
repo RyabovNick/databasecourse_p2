40619dde498d23cdaaebdd260344aa703a2d5601
RyabovNick <nikitos.aka.zed@gmail.com>
2020-11-14 12:09:49 +0300

    style: some more style

--- Lessons/frontend/src/main.js
+++ Lessons/frontend/src/main.js
@@ -7,13 +7,13 @@ import dayjs from 'dayjs'
 Vue.config.productionTip = false
 
 Vue.filter('formatDate', function(value) {
-	if (value) {
-		return dayjs(String(value)).format('DD/MM/YYYY')
-	}
+  if (value) {
+    return dayjs(String(value)).format('DD/MM/YYYY')
+  }
 })
 
 new Vue({
-	router,
-	vuetify,
-	render: (h) => h(App),
+  router,
+  vuetify,
+  render: h => h(App),
 }).$mount('#app')
